aa152881df0e893394ef8fe77da11da92a4099ba
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2019-09-24 14:00:47 +0200

    Implement weight per unit

--- assignment_1.py
+++ assignment_1.py
@@ -47,7 +47,8 @@ def generate_instance(num_items, g, seed):
     # np.random.seed(seed)
     lam = [math.ceil((i + 1)/2) for i in range(num_items)]
     dl = np.minimum(np.random.poisson(lam), 10)
-    dh = [np.random.triangular(90 + g - (i+1), 100 + g - (i+1), 110 + g - (i+1)) for i in range(num_items)]
+    dh = [np.random.triangular(90 + g - (i+1), 100 + g - (i+1), 110 + g - (i+1))
+          for i in range(num_items)]
 
     item_sizes = {"dl": dl, "dh": dh}
 
@@ -104,46 +105,50 @@ p, K, pi, r, item_sizes = skp(num_instances, num_items, g)
 # # Part 2
 
 
-def greedyAlgorithm(problem_instance, pi, r, item_sizes, K):
-    # Compute expectation of w_i for each item
+def greedy_algorithm(problem_instance, pi, r, item_sizes, K):
+    # TODO: Documentation
 
+    # Compute expectation of w_i for each item
     Ew = item_sizes[problem_instance]["dl"] * (np.array(1) - pi) +\
         item_sizes[problem_instance]["dh"] * np.array(pi)
-    alpha = r / Ew
-    alpha_sorted = np.argsort(-alpha)
 
-    # TODO: x should be int
-    x = np.zeros(10)
+    expected_revenue = Ew*r
+    sorted_expected_revenue = np.argsort(expected_revenue)[::-1]
+
+    x = np.zeros(10, dtype=np.int16)
     W = 0
 
-    while len(alpha_sorted) != 0:
-        consider_item = alpha_sorted[0]
+    while len(sorted_expected_revenue) != 0:
+
+        consider_item = sorted_expected_revenue[0]
 
         if W + Ew[consider_item] <= K:
             x[consider_item] = 1
             W = W + Ew[consider_item]
 
-        alpha_sorted = np.delete(alpha_sorted, 0)
+        sorted_expected_revenue = np.delete(sorted_expected_revenue, 0)
 
     return x
 
+
 # Test
-tt = greedyAlgorithm(0, pi, r, item_sizes, K)
+x = greedy_algorithm(0, pi, r, item_sizes, K)
 
 
 # Monte Carlo Simulation
 # # Part 3
+# TODO: put in function
 # np.random.seed(42) # TODO: remove this
 # x = np.random.binomial(1, 0.5, size=num_items)  # TODO: Get these from Martijn's function
 j = 0
-x = greedyAlgorithm(j, pi, r, item_sizes, K)
+x = greedy_algorithm(j, pi, r, item_sizes, K)
 print("x:", x)
 
 # Slide 62 van lecture 1 -> krijg number of runs
 alpha = 0.05
 z = stats.norm.ppf(1-alpha/2)
 epsilon = 0.01
-num_runs = math.ceil((1/2)**2 * (z/epsilon)**2)
+num_runs = math.ceil((1/4) * (z/epsilon)**2)
 
 # num_runs = 20
 
@@ -153,16 +158,19 @@ for run in range(num_runs):
     u = np.random.uniform(size=num_items)
     w = [item_sizes[j]["dl"][i] if u[i] < pi[i] else item_sizes[j]["dh"][i]
          for i in range(num_items)]
+    w = np.asarray(w)
 
     total_weight = np.dot(x, w)
     excess = max(total_weight-K, 0)
 
-    profit = np.dot(x, r) - excess*p
+    profit = np.dot(x, r*w) - excess*p
     profits.append(profit)
 
-print(f"Confidence Interval: [{profits[math.ceil(0.025*num_runs)]}, {profits[math.floor(0.975*num_runs)]}]")
-
 # TODO: Confidence interval
+profits = sorted(profits)
+print(
+    f"Confidence Interval: [{profits[math.ceil(0.025*num_runs)]}, {profits[math.floor(0.975*num_runs)]}]")
+
 
 # Stochastic Programming Models
 # # Part 4
